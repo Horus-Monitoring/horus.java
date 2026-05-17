2bc2c91abbc099a5c5b7ff6e6f871f912db93a98
Jortieke <joao.jortiekejr@sptech.school>
2026-05-16 21:16:47 -0300

    Método para ler o JSON usando JsonNode

--- JarExecutavel/src/main/java/com/sptech/school/service/RelatorioService.java
+++ JarExecutavel/src/main/java/com/sptech/school/service/RelatorioService.java
@@ -1,5 +1,8 @@
 package com.sptech.school.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.*;
@@ -11,15 +14,29 @@ public class RelatorioService {
     }
 
     public Path buscarJSON(){
+        /*Path caminho = Paths.get(
+                "client",
+                "empresa_1",
+                "c0:35:32:c7:0b:59",
+                "dashboard.json"
+        );
+        Para buscar na S3?
+        */
         Path caminho = Paths.get("C:/Users/ricar/Downloads/dashboard (1).json");
         if(Files.exists(caminho)) {
             System.out.println("Arquivo encontrado.");
             return caminho;
         } else {
-            System.out.println("Arquivo não encontrado.");
-            return null;
+            throw new RuntimeException("Arquivo não encontrado.");
         }
     }
+
+    public JsonNode lerJSON(Path caminho) throws IOException {
+        ObjectMapper leitor = new ObjectMapper();
+        return leitor.readTree(caminho.toFile());
+    }
+
+
     /*
 
     buscar o JSON
